5a4e892fa84a485e68b13184b842f7b68329610b
lukechi1219 <luke.chi@gmail.com>
2023-02-17 14:27:15 +0800

    new

---
+++ jmeter/gen_container.sh
@@ -0,0 +1,11 @@
+
+docker run --name="jmeter-v1" --net="host" \
+-v /Users/lukechimbp2023/WorkEnv/jmeter/test/input/jmx:/opt/test/input/jmx \
+-v /Users/lukechimbp2023/WorkEnv/jmeter/test/input/testdata:/opt/test/input/testdata \
+-v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/html:/opt/test/report/html \
+-v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/jtl:/opt/test/report/jtl \
+-v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/outputputdata:/opt/test/report/outputdata \
+-it -d jmeter:v1
+
+#-it -d lukechi1219/jmeter:v1
+
