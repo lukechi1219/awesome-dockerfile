daddbfca7b559c1ccda7a3db1da96f467c501469
lukechi1219 <luke.chi@gmail.com>
2023-02-21 03:06:13 +0800

    revise

--- jmeter/gen_container.sh
+++ jmeter/gen_container.sh
@@ -1,4 +1,7 @@
 
+timestampStr=$(date +'%Y-%m-%d_%H-%M-%S')
+#echo "$timestampStr"
+
 open -a XQuartz
 
 ps aux | grep Xquartz
@@ -7,12 +10,14 @@ xhost +localhost
 
 xhost
 
-docker run --rm -e DISPLAY=host.docker.internal:0 --name=jmeter-v1 --net=host \
+docker run --rm -e DISPLAY=host.docker.internal:0 --name=jmeter-v2 --net=host \
 -v /Users/lukechimbp2023/WorkEnv/jmeter/test/input/jmx:/opt/test/input/jmx \
 -v /Users/lukechimbp2023/WorkEnv/jmeter/test/input/testdata:/opt/test/input/testdata \
 -v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/html:/opt/test/report/html \
 -v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/jtl:/opt/test/report/jtl \
 -v /Users/lukechimbp2023/WorkEnv/jmeter/test/report/outputputdata:/opt/test/report/outputdata \
--it -d jmeter:v1
+-it -d jmeter:v2 \
+jmeter -t /opt/test/input/jmx/BAJI_release_web_homepage.jmx
 
-#-it -d lukechi1219/jmeter:v1
+#jmeter -n -t /opt/test/input/jmx/BAJI_release_web_homepage.jmx -l "/opt/test/report/jtl/$timestampStr.jtl" -e -o "/opt/test/report/html/$timestampStr"
+#ls -alh
