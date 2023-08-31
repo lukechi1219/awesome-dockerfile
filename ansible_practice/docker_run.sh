7547e128dc755aa47170cae8571cc1445ed933c9
lukechi1219 <luke.chi@gmail.com>
2023-08-31 11:55:03 +0800

    update

--- ansible_practice/docker_run.sh
+++ ansible_practice/docker_run.sh
@@ -3,14 +3,19 @@
 # -d, --detach                         Run container in background and print container ID
 # -i, --interactive                    Keep STDIN open even if not attached
 # -t, --tty                            Allocate a pseudo-TTY
-docker run -d -P --name rocky_container_01 ansible_practice_rockylinux
+docker run -d -P --name rocky_container_web_01 ansible_practice_rockylinux
+docker run -d -P --name rocky_container_web_02 ansible_practice_rockylinux
 
-docker run -d -P --name rocky_container_02 ansible_practice_rockylinux
+docker run -d -P --name rocky_container_mg_01 ansible_practice_rockylinux
+docker run -d -P --name rocky_container_mg_02 ansible_practice_rockylinux
+
+docker run -d -P --name rocky_container_api_01 ansible_practice_rockylinux
+docker run -d -P --name rocky_container_api_02 ansible_practice_rockylinux
 
 # The -P flag maps the container's SSH port to a random port on your host. To find out which port it was mapped to, use the following command:
-docker port rocky_container_01 22
-docker port rocky_container_01 8080
+#docker port rocky_container_web_01 22
+#docker port rocky_container_web_01 8080
 
-docker port rocky_container_02 22
-docker port rocky_container_02 8080
+#docker port rocky_container_web_02 22
+#docker port rocky_container_web_02 8080
 
