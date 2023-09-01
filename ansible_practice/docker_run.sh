d89423ac188841073cfa3647f3a1fc1ea0bc99e3
lukechi1219 <luke.chi@gmail.com>
2023-09-01 17:46:10 +0800

    update

--- ansible_practice/docker_run.sh
+++ ansible_practice/docker_run.sh
@@ -14,8 +14,8 @@ docker run -d -P --name rocky_container_api_02 ansible_practice_rockylinux
 
 # The -P flag maps the container's SSH port to a random port on your host. To find out which port it was mapped to, use the following command:
 #docker port rocky_container_web_01 22
-#docker port rocky_container_web_01 8080
+#docker port rocky_container_web_01 80
 
 #docker port rocky_container_web_02 22
-#docker port rocky_container_web_02 8080
+#docker port rocky_container_web_02 80
 
